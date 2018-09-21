4d82d5c194f95f29128842245d98c46e2e861ba2
William Rockwood <wrockwood@tadl.org>
2018-09-21 14:13:35 -0400

    update generate script to support multiple orgs
    Signed-off-by: William Rockwood <wrockwood@tadl.org>

--- generate-tar.sh
+++ generate-tar.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-tar cf tadl-catalog-maintenance.tar maintenance.html
+target=$1
+
+if [ -z $target ]; then
+    echo "You must specify a target. Either kcl, sbbdl or tadl"
+    exit 0
+fi
+
+if [ ! -f ${target}-maintenance.html ]; then
+    echo "That's not a valid target!"
+    exit 0
+fi
+
+echo "Found ${target}-maintenance.html"
+cp ${target}-maintenance.html maintenance.html && tar cf ${target}-maintenance.tar maintenance.html
+
+echo "Ready to deploy to dokku: ${target}-maintenance.tar"
+
